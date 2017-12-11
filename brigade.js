1814aea24ce8c5f4e793216ff6e17c7625099966
Brandon Bethke <brandon.bethke@neudesic.com>
2017-12-11 11:25:55 -0800

    b

--- brigade.js
+++ brigade.js
@@ -3,8 +3,8 @@ const { events, Job } = require("brigadier")
 events.on("timu", () => {
   var job = new Job("do-nothing", "alpine:3.4")
   job.tasks = [
-    "cd /src"
-    "ls -al"
+    "cd /src",
+    "ls -al",
     "echo Hello",
     "echo Timu"
   ]
